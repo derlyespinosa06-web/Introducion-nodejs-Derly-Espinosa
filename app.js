8400e479f085e2b611665e9b23c3cac71e03bd21
Derly Maday Espinosa Cruz <derlyespinosa06@gmail.com>
2026-07-23 17:34:54 -0500

    entrega de 3030

--- app.js
+++ app.js
@@ -1,12 +1,11 @@
-import express from "express";
-const app = express();
-
-const port = 3000;
-
-app.get("/", (req, res) => {
-    res.send("Hola, estamos aprendiendo Express con la ficha 3407184");
-});
-
-app.listen(port, () => {
-    console.log(`Servidor en funcionamiento en el puerto: ${port}`);
+import express from 'express';
+import {configDotenv} from "dotenv"
+configDotenv()
+const app = express();  
+const port = process.env.PORT || 3000; 
+app.get("/", (_, res) => { 
+res.send("Hola , estamos aprendiendo express con la ficha 3407184"); 
+});  
+app.listen(port, () => { 
+console.log( `Servidor en funcionamiento en el puerto ${port}  `); 
 });
