06f6462a276db96b6f03d0f34b48861628554d69
derly <derlyespinosa06@gmail.com>
2026-08-05 23:37:38 -0500

    se agrega producto

--- app.js
+++ app.js
@@ -1,8 +1,38 @@
 import express from 'express';
-import {configDotenv} from "dotenv"
+import {configDotenv} from "dotenv";
+import cors from "cors";
+
+// Const bodyParser = require(`body-Parser`);//importacion commonjs
+import bodyParser from "body-parser"; //Importacion ES "module"
 configDotenv()
+
 const app = express();  
-const port = process.env.PORT || 3000; 
+const port = process.env.PORT || 3000; // Cambiado a 3030 por tu requerimiento previo ya que antes estada en 3000
+
+// Middlewares para procesar datos JSON y formularios
+app.use(cors());
+app.use(bodyParser.json());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+export const Saludo = "Hola, estamos aprendiendo express con la ficha 3407184";
+
+app.get("/", (req, res) => {
+    res.send(Saludo);
+});
+
+app.get("/productos", (req, res) => {
+    const orden = req.query.orden || "Sin orden"
+    const pagina = req.query.pagina || "Sin pagina"
+    res.send(` <h1> Listado de Productos en orden ${orden} en la pagina ${pagina} </h1>
+        <ul>
+            <li>Televisor</li>
+            <li>Celular</li>
+            <li>Impresora</li>
+        </ul>
+    `);
+});
+
 app.get("/", (_, res) => { 
 res.send("Hola , estamos aprendiendo express con la ficha 3407184"); 
 });  
